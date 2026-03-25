7f1a0cc2a514779173275a49929747062f9078e4
Albedo-AB <di.aravenab@duocuc.cl>
2026-03-25 17:40:15 -0300

    Agregado de dockerfile y eurekaserver

---
+++ ms-eureka-server/src/main/java/com/duoc/ms_eureka_server/MsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.ms_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableEurekaServer
+@SpringBootApplication
+public class MsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEurekaServerApplication.class, args);
+	}
+
+}
