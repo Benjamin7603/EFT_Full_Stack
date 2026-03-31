4d5a9d699709504a5ae4e899dc9745ab5bda7823
carlitroxxx <ca.moil@duocuc.cl>
2026-03-31 17:17:18 -0300

    Valid integrado al ms usuario

--- ms-usuarios/src/main/java/com/duoc/ms_usuarios/model/Usuario.java
+++ ms-usuarios/src/main/java/com/duoc/ms_usuarios/model/Usuario.java
@@ -4,6 +4,9 @@ package com.duoc.ms_usuarios.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,8 +21,13 @@ public class Usuario {
     @Id
     @GeneratedValue
     private Long id;
+    @NotBlank(message = "El nombre es obligatorio.") //mensaje solo backend
+    @Size(min=3, max=20,message = "El nombre debe tener entre 3 y 20 caracteres.")
     private String nombre;
     private String apellido;
+    //Debe tener un formato de email
+    @Email(message = "El correo no tiene con un formato valido")
+    @NotBlank(message = "El correo es un campo obligatorio")
     private String email;
     private String telefono;
     private Date fechaNacimiento;
