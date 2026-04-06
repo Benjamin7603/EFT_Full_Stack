e3c831821b6d0df5f619db50c5ad2a02edad5205
Benjamin7603 <bgdiaz2006@gmail.com>
2026-04-05 22:31:07 -0400

    nuevo avance de proyetcto swagger correcto, problemas con gateway

--- ms-gateway/src/test/java/com/duoc/ms_gateway/MsGatewayApplicationTests.java
+++ ms-gateway/src/test/java/com/duoc/ms_gateway/MsGatewayApplicationTests.java
@@ -2,8 +2,10 @@ package com.duoc.ms_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Esto le dice que use el archivo application-test.yml
 class MsGatewayApplicationTests {
 
 	@Test
