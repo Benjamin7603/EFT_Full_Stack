9178a64165218b81a8e1bee25fdc6180da8a8641
carlitroxxx <ca.moil@duocuc.cl>
2026-04-30 02:53:35 -0400

    FIX

--- ms-bff/src/main/java/com/duoc/ms_bff/client/GeograficoClient.java
+++ ms-bff/src/main/java/com/duoc/ms_bff/client/GeograficoClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-geografico")
+@FeignClient(name = "ms-geografico", url = "${MS_GEOGRAFICO_URL}")
 public interface GeograficoClient {
 
     @GetMapping("/api/geografico/reporte/{idReporte}")
