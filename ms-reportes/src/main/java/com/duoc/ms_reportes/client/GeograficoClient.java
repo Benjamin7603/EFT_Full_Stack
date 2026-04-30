6840203e751fec388ad0b3a1d0047eb40a58dff6
carlitroxxx <ca.moil@duocuc.cl>
2026-04-30 03:02:47 -0400

    FIX

--- ms-reportes/src/main/java/com/duoc/ms_reportes/client/GeograficoClient.java
+++ ms-reportes/src/main/java/com/duoc/ms_reportes/client/GeograficoClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "MS-GEOGRAFICO")
+@FeignClient(name = "MS-GEOGRAFICO", url = "${MS_GEOGRAFICO_URL}")
 public interface GeograficoClient {
 
     @PostMapping("/api/geografico/guardar")
--- ms-reportes/src/main/java/com/duoc/ms_reportes/client/NotificacionClient.java
+++ ms-reportes/src/main/java/com/duoc/ms_reportes/client/NotificacionClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "MS-NOTIFICACIONES")
+@FeignClient(name = "MS-NOTIFICACIONES", url = "${MS_NOTIFICACIONES_URL}")
 public interface NotificacionClient {
 
     @PostMapping("/api/notificaciones/enviar")
